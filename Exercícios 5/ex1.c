23153e8ae42ba9c216156839e1f4e877e915faf3
pedrocavadas <43155616+pedrocavadas@users.noreply.github.com>
2018-10-08 09:35:25 -0300

    Add files via upload

---
+++ Exercícios 5/ex1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void pont(int* p){
+	*p=(*p-50);
+}
+
+int main(){
+    int a,b;
+    int* p;
+    printf ("Digite um numero: ");
+    scanf ("%d", &a);
+    printf ("Digite outro numero: ");
+    scanf ("%d", &b);
+    printf ("A=%d B=%d\n", a,b);
+    if (a>b){
+        p=&a;
+	    pont(p);
+	    printf ("NOVO VALOR\nA=%d B=%d\n", *p,b);
+    }
+    else{
+        p=&b;
+	    pont(p);
+	    printf ("NOVO VALOR\nA=%d B=%d\n", a,*p);
+    }
+}
