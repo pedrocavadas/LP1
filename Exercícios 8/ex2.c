42e7daaa5b8a9835f4cd3ee76a6723375ef98794
pedrocavadas <43155616+pedrocavadas@users.noreply.github.com>
2018-12-10 09:42:43 -0200

    Create ex2.c

---
+++ Exercícios 8/ex2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int i=0;
+    char v[25];
+    FILE* f=fopen("/tmp/arq-01.txt","r");
+    v[i]=fgetc(f);
+    while (v[i]!=EOF){
+        i++;
+        frase[i]=fgetc(f);
+    }
+    frase[i]='\0';
+    fclose(f);
+    printf("%s",frase);
+}
