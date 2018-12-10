0c377df32e375e5ba78a79a8a6bdd4dc112dcf05
pedrocavadas <43155616+pedrocavadas@users.noreply.github.com>
2018-12-10 09:43:08 -0200

    Create ex3.c

---
+++ Exercícios 8/ex3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int i, j;
+    char v[10][25];
+    FILE* f=fopen("/tmp/arq-02.txt", "w");
+    for (i=0;i<10;i++){
+    	printf("Digite uma frase: ");
+        scanf("%s", v[i]);
+    }
+    for (i=0;i<10;i++){
+        for (j=0;j<strlen(v[i]);j++){
+	    fputc(v[i][j],f);
+	}
+	fputc(' ',f);
+    }
+    fclose(f);
+}
