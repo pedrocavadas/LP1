27b59d9ccc0e7e95db1ca02767f6acf92f6ae004
pedrocavadas <43155616+pedrocavadas@users.noreply.github.com>
2018-12-15 15:00:48 -0200

    Update ex3.c

--- Exercícios 8/ex3.c
+++ Exercícios 8/ex3.c
@@ -5,10 +5,10 @@
 int main(){
     int i, j;
     char v[10][25];
-    FILE* f=fopen("/tmp/arq-02.txt", "w");
+    FILE* f=fopen("D:/Dev-Cpp/Programas feitos/arq.txt", "w");
     for (i=0;i<10;i++){
     	printf("Digite uma frase: ");
-        scanf("%s", v[i]);
+        fgets(v[i],25,stdin);
     }
     for (i=0;i<10;i++){
         for (j=0;j<strlen(v[i]);j++){
