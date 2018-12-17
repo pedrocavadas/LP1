3a534767ff4609228e7e3d79224164a55651651b
pedrocavadas <43155616+pedrocavadas@users.noreply.github.com>
2018-12-17 10:19:04 -0200

    Add files via upload

---
+++ Exercícios 9/ex3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Caixa{
+	int valor;
+	struct Caixa* prox;
+};
+
+void imprime(struct Caixa* caixa){
+	while (caixa!=NULL){
+		printf("%d->", caixa->valor);
+		caixa=caixa->prox;
+	}
+}
+
+int main(){
+	struct Caixa c1,c2,c3,c4,c5;
+	
+	printf("Digite o valor da primeira caixa: ");
+	scanf("%d", &c1.valor);
+	c1.prox=&c2;
+
+	printf("Digite o valor da segunda caixa: ");
+	scanf("%d", &c2.valor);
+	c2.prox=&c3;
+	
+	printf("Digite o valor da terceira caixa: ");
+	scanf("%d", &c3.valor);
+	c3.prox=&c4;
+
+	printf("Digite o valor da quarta caixa: ");
+	scanf("%d", &c4.valor);
+	c4.prox=&c5;
+
+	printf("Digite o valor da quinta caixa: ");
+	scanf("%d", &c5.valor);
+	c5.prox=NULL;
+
+	imprime(&c1);
+}
+	
